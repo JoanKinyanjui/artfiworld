f19e6de58f85e6f7cdfe196108e7dbe64c8f5623
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2022-10-20 01:58:23 +0300

    ..

--- src/components/BOXES/Boxes.js
+++ src/components/BOXES/Boxes.js
@@ -7,25 +7,25 @@ function BoxesDiv() {
   return (
     <Container maxWidth="xl" style={{paddingTop:"172px"}} >
 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 1 }}>
-  <Grid item xs={6} sm={3} >
+  <Grid item xs={6} md={3} className="BoxGrid" >
     <Stack className="BoxesStack" >
         <Typography className='Numbers'>50+</Typography>
         <Typography className="Sentence" >Outlets</Typography>
     </Stack>
   </Grid>
-  <Grid item xs={6} sm={3} >
+  <Grid item xs={6} md={3} className="BoxGrid" >
   <Stack className="BoxesStack" >
         <Typography className='Numbers1'>100k+</Typography>
         <Typography className="Sentence">Students</Typography>
     </Stack>
   </Grid>
-  <Grid item xs={6} sm={3} >
+  <Grid item xs={6} md={3} className="BoxGrid">
   <Stack className="BoxesStack" >
         <Typography className='Numbers'>200+</Typography>
         <Typography className="Sentence">Trainers</Typography>
     </Stack>
   </Grid>
-  <Grid item xs={6} sm={3} >
+  <Grid item xs={6} md={3} className="BoxGrid" >
   <Stack className="BoxesStack" >
         <Typography className='Numbers'>50k+</Typography>
         <Typography className="Sentence">Members</Typography>
