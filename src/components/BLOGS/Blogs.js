17f062ec4e41b516fcc06c03e912e5213f42fa69
JoanKinyanjui <kinyanjuijoan52@gmail.com>
2022-10-20 12:52:33 +0300

    ..

--- src/components/BLOGS/Blogs.js
+++ src/components/BLOGS/Blogs.js
@@ -23,7 +23,7 @@ function Blogs() {
                     </Box>
                 </Grid>
 
-                <Grid item xs={6} className='grid2' sx={{gap:'20px'}}>
+                <Grid item xs={12} sm={6} className='grid2' sx={{gap:'20px'}}>
                     <Stack sx={{gap:'40px'}} className="SmallBlogs">
                     <Box className='Blog2'>
                         <Stack className='textgallery1'>
